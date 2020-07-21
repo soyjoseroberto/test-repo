fd52cae8a3e085f5ef18a9e8d6e5d49296d65e75
soyjoseroberto <jrodriguez18@gmail.com>
2020-07-21 16:35:16 -0400

    Working with classes.

--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,33 @@
-// Code goes here!
-// Push this change
+class Department {
+    name: string;
+    employees: string[] = [];
+
+    constructor(n: string) {
+        this.name = n;
+    }
+
+    describe() {
+        console.log('Department: ' + this.name);
+    }
+
+    addEmployee(employee: string) {
+        this.employees.push(employee);
+    }
+
+    printEmployeeInformation() {
+        console.log(this.employees.length);
+        console.log(this.employees);
+    }
+}
+
+const accounting = new Department('Accounting');
+console.log(accounting);
+
+accounting.addEmployee('Jose');
+accounting.addEmployee('Arielle');
+
+accounting.printEmployeeInformation();
+// Talking about 'this'
+const accountingCopy = { describe: accounting.describe } // not executing the function
+accounting.describe(); // this yields undefined since 'this.name' does not exist in accountingCopy
+
